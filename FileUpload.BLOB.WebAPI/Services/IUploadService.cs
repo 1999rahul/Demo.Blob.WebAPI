7e94ba99c4d39eb603c9447af33b186c37210c80
Rahul Kumar <1999rahul.py@gmail.com>
2023-02-15 18:11:55 +0530

    Init

---
+++ FileUpload.BLOB.WebAPI/Services/IUploadService.cs
@@ -0,0 +1,7 @@
+﻿namespace FileUpload.BLOB.WebAPI.Services
+{
+    public interface IUploadService
+    {
+        Task<string> UploadAsync(Stream fileStream, string fileName, string contentType);
+    }
+}
---
+++ FileUpload.BLOB.WebAPI/Services/UploadService.cs
@@ -0,0 +1,27 @@
+﻿using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs;
+
+namespace FileUpload.BLOB.WebAPI.Services
+{
+    public class UploadService: IUploadService
+    {
+        private readonly string _storageConnectionString;
+        public UploadService(IConfiguration configuration)
+        {
+            _storageConnectionString = configuration.GetConnectionString("AzureStorageCnnString");
+        }
+        public async Task<string> UploadAsync(Stream fileStream, string fileName, string contentType)
+        {
+            var container = new BlobContainerClient(_storageConnectionString, "file-container");
+            var createResponse = await container.CreateIfNotExistsAsync();
+
+            if (createResponse != null && createResponse.GetRawResponse().Status == 201)
+                await container.SetAccessPolicyAsync(PublicAccessType.Blob);
+            var blob = container.GetBlobClient(fileName);
+            await blob.DeleteIfExistsAsync(DeleteSnapshotsOption.IncludeSnapshots);
+            await blob.UploadAsync(fileStream, new BlobHttpHeaders { ContentType = contentType });
+            return blob.Uri.ToString();
+        }
+
+    }
+}
